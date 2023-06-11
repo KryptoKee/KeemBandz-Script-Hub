11f84a5ad0e5b57e43c3182c2c0ac6b48d96e684
KryptoKee <93107101+KryptoKee@users.noreply.github.com>
2023-06-11 01:32:15 -0400

    Update street-outlaws.lua
    Added a race sniper! Choose car and bet then click "Join Race" when its time. Be careful spamming it.

--- street-outlaws.lua
+++ street-outlaws.lua
@@ -1,49 +1,29 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("KeemBandz Street Outlaws GUI", "Ocean")
 
--- MONEY
+
+
+--MONEY
+
+
 local Main = Window:NewTab("Money")
 local MainSection = Main:NewSection("Money")
 
-local userInputMoney = 0
-
-MainSection:NewTextBox("Enter Money", "Type amount then click 'Add Money'", function(txt)
-	local num = tonumber(txt)
-	if num then
-		userInputMoney = num
-	else
-		print("Invalid input, please enter a number.")
-	end
+MainSection:NewDropdown("Add Money", "Select amount to add", {"100000", "250000", "1000000", "10000000"}, function(currentOption)
+    game:GetService("ReplicatedStorage"):WaitForChild("WinningsAdd"):FireServer(currentOption)
 end)
 
-MainSection:NewButton("Add Money", "Give entered money", function()
-	if userInputMoney > 0 then
-		local args = {
-			[1] = userInputMoney
-		}
-		game:GetService("ReplicatedStorage"):WaitForChild("WinningsAdd"):FireServer(unpack(args))
-	else
-		print("Please enter a valid amount of money.")
-	end
-end)
-
-MainSection:NewButton("Remove Money", "Remove entered money", function()
-	if userInputMoney > 0 then
-		local args = {
-			[1] = -userInputMoney 
-		}
-		game:GetService("ReplicatedStorage"):WaitForChild("WinningsAdd"):FireServer(unpack(args))
-	else
-		print("Please enter a valid amount of money.")
-	end
+MainSection:NewDropdown("Remove Money", "Select amount to remove", {"-100000", "-250000", "-1000000", "-10000000"}, function(currentOption)
+    game:GetService("ReplicatedStorage"):WaitForChild("WinningsAdd"):FireServer(currentOption)
 end)
 
 
 --SELF
-	
+
+
 local Main = Window:NewTab("Self")
 local PlayerSection = Main:NewSection("Self")
-    
+
 PlayerSection:NewSlider("Walkspeed", "SPEED!!", 500, 16, function(s)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
@@ -52,30 +32,60 @@ PlayerSection:NewButton("Reset WS", "Resets to default", function()
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 end)
-    
+
+
+--RACE
+
+local RaceSection = Main:NewSection("Race")
+
+local car
+RaceSection:NewDropdown("Choose Car", "Select your car", {"FoxBodyMustang1990", "FoxBodyMustang1992", "ChevroletElCamino", "ChevroletBlazerBlown", "ChevroletCamaro1969", "ChevroletCamaro1989", "ChevroletNova1967", "ChevroletS10", "ChevroletImpalaSS", "ChevroletBelair", "DodgeChargerRT", "MazdaRX7", "ToyotaPickup"}, function(currentOption)
+    car = currentOption
+end)
+
+local color
+RaceSection:NewDropdown("Choose Color", "Color of your car", {"Gray", "White", "Blue", "Red", "Purple", "Brown", "Yellow", "Green"}, function(currentOption)
+    color = currentOption
+end)
+
+local bet
+RaceSection:NewDropdown("Bet Amount", "Select your bet", {"100", "500", "1000", "2000", "5000", "10000", "50000"}, function(currentOption)
+    bet = tonumber(currentOption)
+end)
+
+RaceSection:NewButton("Join Race", "Attempt to join race", function()
+    if car and color and bet then
+        game:GetService("ReplicatedStorage"):WaitForChild("WinningsHandler"):FireServer()
+        wait(0.2)
+
+        local args = {
+            [1] = car .. ":" .. color,
+            [2] = bet
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("PlayerCarStart"):FireServer(unpack(args))
+    else
+        print("Please select a car, color, and bet amount.")
+    end
+end)
+
 --DONATE
+
 
 local Main = Window:NewTab("Donate")
 local GiveSection = Main:NewSection("Give")
 
-local donateAmount = 0 -- This variable will hold the value to donate
+local donateAmount
 
 GiveSection:NewTextBox("Donation Amount", "Type the amount to donate", function(txt)
-    local num = tonumber(txt)
-    if num then
-        donateAmount = num -- If the input is a valid number, store it in the variable
-    else
-        print("Invalid input, please enter a number.")
-    end
+    donateAmount = parseNumber(txt)
 end)
 
--- Create a button for each player currently in the game
 for _, player in ipairs(game.Players:GetPlayers()) do
     GiveSection:NewButton("Donate to " .. player.Name, "Donate entered amount to " .. player.Name, function()
-        if donateAmount > 0 then
+        if donateAmount then
             local args = {
-                [1] = donateAmount, -- use this value to donate money
-                [2] = player.UserId  -- use this UserId to specify the receiver
+                [1] = donateAmount,
+                [2] = player.UserId
             }
             game:GetService("ReplicatedStorage"):WaitForChild("GiveDonation"):FireServer(unpack(args))
         else
@@ -83,3 +93,17 @@ for _, player in ipairs(game.Players:GetPlayers()) do
         end
     end)
 end
+
+
+--OTHER
+
+local Main = Window:NewTab("Other")
+local OtherSection = Main:NewSection("Other")
+
+OtherSection:NewButton("Reconnect to Server", "Rejoin the current server", function()
+    local teleportService = game:GetService("TeleportService")
+    local placeId = game.PlaceId
+    local currentServerId = game.JobId
+
+    teleportService:TeleportToPlaceInstance(placeId, currentServerId)
+end)
