a3707ea1d27f50bf1f8facf60aa4032f4807b15a
KryptoKee <93107101+KryptoKee@users.noreply.github.com>
2023-06-14 00:51:53 -0400

    Update hoop-simulator.lua

--- hoop-simulator.lua
+++ hoop-simulator.lua
@@ -1,19 +1,24 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
--- Create the GUI
 local Window = Library.CreateLib("KeemBandz Hoop Simulator Auto Farm", "Synapse")
 local Tab = Window:NewTab("Main")
 local MainSection = Tab:NewSection("Settings")
 
+local farmingEnabled = false
+
 MainSection:NewToggle("Auto Farm", "Stand within range of hoop to farm", function(state)
-    if state then
-        local A_1 = 1
-        local Event = game:GetService("ReplicatedStorage")["events-shared/core/events.module@GlobalEvents"].throwBall
-        Event:FireServer(A_1)        
+    farmingEnabled = state
+    
+    if farmingEnabled then
+        while farmingEnabled do
+            local A_1 = 1
+            local Event = game:GetService("ReplicatedStorage")["events-shared/core/events.module@GlobalEvents"].throwBall
+            Event:FireServer(A_1)
+            wait(3)
+        end
     else
         print("Toggle Off")
     end
 end)
 
--- Start the loop
 Library:Init()
